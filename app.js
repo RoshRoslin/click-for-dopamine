c61f9ff5955897074667fcd4cab653f3bb85778f
RoshRoslin <RoshRoslin@gmail.com>
2020-12-15 13:00:57 -0800

    center align image

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ button.addEventListener('click', function(){
   let random2 = Math.floor(Math.random() * 10);
 
   div.innerHTML = `
-    <img class = "responsive-img z-depth-5" src="https://source.unsplash.com/80${random1}x60${random2}/?cat,dog,puppy,kitten" alt="">
+    <img class = "col s12 responsive-img z-depth-5" src="https://source.unsplash.com/80${random1}x60${random2}/?cat,dog,puppy,kitten" alt="">
   `
 
 });
